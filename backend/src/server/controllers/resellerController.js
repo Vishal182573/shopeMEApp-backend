96315a23284d419b9df235fb9a477204f4452f9b
Vishal Sharma <sharmavs9205@gmail.com>
2024-07-05 22:41:44 +0530

    resolved error in backend

--- backend/src/server/controllers/resellerController.js
+++ backend/src/server/controllers/resellerController.js
@@ -38,15 +38,14 @@ const resellerRegistration = asyncHandler(async (req, res) => {
     const payload = {
       reseller: {
         id: savedReseller._id,
-      },
-      type:"reseller"
+      }
     };
 
     jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
       if (err) {
         return res.status(500).json({ message: "Token generation failed" });
       }
-      res.status(200).json({ token, message: "Registration successful" });
+      res.status(200).json({ token, message: "Registration successful" ,session:{id: savedReseller._id,type:"reseller"}});
     });
 
   } catch (err) {
@@ -79,7 +78,7 @@ const resellerLogin = asyncHandler(async (req, res) => {
         if (err) {
           return res.status(500).json({ message: "Token generation failed" });
         }
-        return res.status(200).json({ token, message: "Login successful" });
+        return res.status(200).json({ token, message: "Login successful" ,session:{id: savedReseller._id,type:"reseller"}});
       });
     } else {
       return res.status(401).json({ message: "Incorrect password" });
