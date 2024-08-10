49d3a72bd54f5827a7c1f8789c530f770d1188bc
vipin126 <chandvipin68@gmail.com>
2024-08-10 23:16:27 +0530

    messsage changes

--- backend/src/server/routes/requirementRoutes.js
+++ backend/src/server/routes/requirementRoutes.js
@@ -7,7 +7,7 @@ import {
     searchrequirement,
     getRequirementByUserId
 } from "../controllers/requirementsControllers.js"
-import auth from "../middleware/authMiddleware.js"
+//import auth from "../middleware/authMiddleware.js"
 
 const router = Router();
 
@@ -15,7 +15,7 @@ router.post("/postRequirement",postRequirements);
 
 router.get("/getAllRequirements",showAllRequirements);
 router.get("/getRequirementsByCategory",getRequirementByCategory),
-router.get("/deleteRequirement",auth,deleteRequirement);
+router.get("/deleteRequirement",deleteRequirement);
 router.get("/searchRequirement",searchrequirement);
 router.get("/getReqByUserid",getRequirementByUserId);
 
